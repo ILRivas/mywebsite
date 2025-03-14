d0650f0513fab2030f2181e83fbe9358b24cc025
Israel Rivas <rivas@Mac.lan>
2025-03-13 21:36:19 -0500

    Removed old JPG/PNG images after webP conversion

--- src/utils/importImages.ts
+++ src/utils/importImages.ts
@@ -1,7 +1,8 @@
 const importAll = (r: __WebpackModuleApi.RequireContext) => r.keys().map(r);
 
-export const featuredWork = importAll(require.context('../assets/Photography/Featured_Work', false, /\.(png|jpe?g|svg)$/));
-export const candidPortraits = importAll(require.context('../assets/Photography/Candid_Portraits_Moments', false, /\.(png|jpe?g|svg)$/));
-export const foodPhotography = importAll(require.context('../assets/Photography/Food_Photography', false, /\.(png|jpe?g|svg)$/));
-export const graduations = importAll(require.context('../assets/Photography/Graduations', false, /\.(png|jpe?g|svg)$/));
-export const weddings = importAll(require.context('../assets/Photography/Weddings_Domestic_Partnerships', false, /\.(png|jpe?g|svg)$/));
+// ✅ Update paths to use the WebP folder & match only .webp files
+export const featuredWork = importAll(require.context('../assets/Photography_WebP/Featured_Work', false, /\.(webp)$/));
+export const candidPortraits = importAll(require.context('../assets/Photography_WebP/Candid_Portraits_Moments', false, /\.(webp)$/));
+export const foodPhotography = importAll(require.context('../assets/Photography_WebP/Food_Photography', false, /\.(webp)$/));
+export const graduations = importAll(require.context('../assets/Photography_WebP/Graduations', false, /\.(webp)$/));
+export const weddings = importAll(require.context('../assets/Photography_WebP/Weddings_Domestic_Partnerships', false, /\.(webp)$/));
